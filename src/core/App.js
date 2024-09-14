ab93d911f3ebbc47f33f32415f3d86a848c84c67
Przemysław Pacek <ppacek@op.pl>
2024-09-14 22:53:05 +0200

    App file changed

---
+++ src/core/App.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+import { Footer } from '../features/homepage/Footer';
+import { Gallery } from '../features/homepage/Gallery';
+import { Headers } from '../features/homepage/Headers';
+import { Hero } from '../features/homepage/Hero';
+import { Navigations } from '../features/homepage/Navigations';
+import { Seo } from '../features/homepage/Seo';
+import './App.css';
+
+function App() {
+  const [activeTab, setActiveTab] = useState("osobowe");
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <>
+      <Navigations />
+      <Hero />
+      <Headers activeTab={activeTab} setActiveTab={setActiveTab} />
+      <Gallery activeTab={activeTab} />
+      <Seo />
+      <Footer />
+    </>
+  )
+};
+
+export default App;
