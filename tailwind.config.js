4e403e71dc9c4f5c02acac93bfc3a9c01ad886e9
Przemysław Pacek <ppacek@op.pl>
2024-09-14 22:40:48 +0200

    installed tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        robotoFlex: ['Roboto Flex', 'sans-serif'],
+        robotoCond: ['Roboto Condensed', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
