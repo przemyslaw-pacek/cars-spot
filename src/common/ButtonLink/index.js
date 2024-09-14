5f856ebd9f2f2f4a95b03ba5dbe9c5bb87298639
Przemysław Pacek <ppacek@op.pl>
2024-09-14 22:48:05 +0200

    ButtonLink component created

---
+++ src/common/ButtonLink/index.js
@@ -0,0 +1,26 @@
+export const ButtonLink = ({
+    link = "tel:000000000",
+    text = "Zadzwoń do nas",
+    bgColor = "#0147FF",
+    textColor = "#F7F7F7"
+}) => (
+    <a href={link}
+        className="
+            text-[12px]
+            sm:text-[15px]
+            font-semibold
+            px-2.5
+            sm:px-4
+            xl:px-6
+            py-1.5
+            sm:py-2
+            xl:py-3
+            tracking-[-0.02em]
+            rounded-[8px]
+            border-[1px]
+            border-[#0147FF]"
+        style={{ backgroundColor: bgColor, color: textColor }}
+    >
+        {text}
+    </a>
+);
