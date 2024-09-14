397ccbe7d8f11d3f8d3fb3f65acfb6a6d59de514
Przemysław Pacek <ppacek@op.pl>
2024-09-14 22:57:07 +0200

    Footer component created

---
+++ src/features/homepage/Footer/index.js
@@ -0,0 +1,27 @@
+export const Footer = () => (
+    <footer
+        className="
+            font-robotoCond
+            bg-[#282828]
+            text-white
+            px-[30px]
+            sm:px-[60px]
+            xl:px-[120px]
+            py-4
+            sm:py-5
+            lg:py-6
+            flex
+            justify-between
+            border-t"
+    >
+        <div className="font-bold">
+            Cars Spot
+        </div>
+        <a href="https://example.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="font-normal underline">
+            Polityka prywatności
+        </a>
+    </footer>
+);
