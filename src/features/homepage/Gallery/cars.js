90ea6f2083abf6f60b8e59c8e8e69b5e83553bd7
Przemysław Pacek <ppacek@op.pl>
2024-09-14 22:58:01 +0200

    car database created

---
+++ src/features/homepage/Gallery/cars.js
@@ -0,0 +1,9 @@
+import car1 from "./images/car_1.jpg";
+import car2 from "./images/car_2.jpg";
+import car3 from "./images/car_3.jpg";
+import car4 from "./images/car_1.jpg";
+import car5 from "./images/car_2.jpg";
+
+export const passengerCars = [car1, car2, car3, car4, car5];
+
+export const deliveryVans = [car5, car4, car3, car2];
